c160d11a6909160453137a471259c95563ae300c
Aaron Jones <aaron@jonesinator.com>
2017-09-23 17:32:33 -0600

    Use git tags for version numbers.

--- doc/conf.py
+++ doc/conf.py
@@ -1,6 +1,26 @@
-#!/usr/bin/env python3
+""" Configuration file for Sphinx documentation generation. """
 import os
+import shlex
+import subprocess
 import sys
+
+
+def run(cmd):
+    """ Simple helpter to run a shell command and get the output. """
+    return subprocess.check_output(shlex.split(cmd)).decode('utf-8').strip()
+
+
+# If GIT_DIR is set we're in the pre-commit hook, and won't be in the directory
+# with .git. Temporarily delete the GIT_DIR environment variable so we can run
+# the git commands required to get the version number and then restore the
+# value.
+old_val = os.environ['GIT_DIR'] if 'GIT_DIR' in os.environ else None
+if old_val:
+    del os.environ['GIT_DIR']
+version = release = run('git describe')
+if old_val:
+    os.environ['GIT_DIR'] = old_val
+
 sys.path.insert(0, os.path.abspath('..'))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.githubpages']
 source_suffix = '.rst'
@@ -8,20 +28,8 @@ master_doc = 'index'
 project = 'muse_usps'
 copyright = '2017, Aaron Jones'
 author = 'Aaron Jones'
-version = '0.0.1'
-release = '0.0.1'
 language = None
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 todo_include_todos = False
 html_theme = 'sphinx_rtd_theme'
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-        'donate.html',
-    ]
-}
 htmlhelp_basename = 'muse_uspsdoc'
--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 """ Packaging script for muse_usps."""
+import shlex
+import subprocess
+
 import setuptools
+
+
+def run(cmd):
+    """ Simple helpter to run a shell command and get the output. """
+    return subprocess.check_output(shlex.split(cmd)).decode('utf-8').strip()
+
+
 setuptools.setup(name='muse_usps',
-                 version='0.0.1',
+                 version=run('git describe --abbrev=0'),
                  description='USPS Address Verification',
                  install_requires=['lxml'],
                  packages=['muse_usps'])
